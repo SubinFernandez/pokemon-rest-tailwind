555a192aced58fe6b8a107ab3a885650eb70d631
Subin Fernandez <Subin.Fernandez@gmail.com>
2021-10-19 17:07:26 +0100

    Fixed the auto-select dropdown issue

--- src/components/pokemon-filter/pokemon-filter.tsx
+++ src/components/pokemon-filter/pokemon-filter.tsx
@@ -5,160 +5,230 @@ import debounce from 'lodash/debounce'
 import classnames from 'classnames'
 import { FilterIcon } from '@heroicons/react/outline'
 
-import { Ability, NamedAPIResource, NamedAPIResourceList } from '@src/types/pokemon.type'
+import {
+  Ability,
+  NamedAPIResource,
+  NamedAPIResourceList,
+} from '@src/types/pokemon.type'
 import { LOCAL_STORAGE_KYES, REST_API } from '../../helpers/constants'
 
 interface PokemonFilterProps {
   onFilter: (pokemons: NamedAPIResource[] | undefined) => void
 }
 
-export const PokemonFilter: React.FC<PokemonFilterProps> = memo(({ onFilter }) => {
-  const [pokemons, setPokemons] = useState<NamedAPIResource[]>()
-  const [abilities, setAbilities] = useState<NamedAPIResource[]>()
-  const [selectedPokemon, setSelectedPokemon] = useState('')
-  const [selectedAbility, setSelectedAbility] = useState('')
-  const [filteredPokemons, setFilteredPokemons] = useState<NamedAPIResource[]>([])
-  const [{ data: dataPokemons }] = useAxios(`${REST_API.url}/${REST_API.endpoints.pokemon}?limit=-1`)
-  const [{ data: dataAbilities }] = useAxios(`${REST_API.url}/${REST_API.endpoints.ability}?limit=-1`)
-  const [{ data: dataPokemonsOfAbility }, fetchPokemonsWithAbility] = useAxios(`${REST_API.url}/${REST_API.endpoints.ability}/${selectedAbility}`, { manual: true })
+export const PokemonFilter: React.FC<PokemonFilterProps> = memo(
+  ({ onFilter }) => {
+    const [pokemons, setPokemons] = useState<NamedAPIResource[]>()
+    const [abilities, setAbilities] = useState<NamedAPIResource[]>()
+    const [selectedPokemon, setSelectedPokemon] = useState('')
+    const [selectedAbility, setSelectedAbility] = useState('')
+    const [filteredPokemons, setFilteredPokemons] = useState<
+      NamedAPIResource[]
+    >([])
+    const [{ data: dataPokemons }] = useAxios(
+      `${REST_API.url}/${REST_API.endpoints.pokemon}?limit=-1`,
+    )
+    const [{ data: dataAbilities }] = useAxios(
+      `${REST_API.url}/${REST_API.endpoints.ability}?limit=-1`,
+    )
+    const [{ data: dataPokemonsOfAbility }, fetchPokemonsWithAbility] =
+      useAxios(
+        `${REST_API.url}/${REST_API.endpoints.ability}/${selectedAbility}`,
+        { manual: true },
+      )
 
-  const applyFilter = useCallback(debounce(() => {
-    if (selectedPokemon.length && pokemons) {
-      setFilteredPokemons(pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(selectedPokemon.toLowerCase())))
-    } else if (selectedAbility.length && abilities) {
-      if (abilities.find(ability => ability.name.toLowerCase() === selectedAbility.toLowerCase())) {
-        fetchPokemonsWithAbility()
+    const applyFilter = useCallback(
+      debounce(() => {
+        if (selectedPokemon.length && pokemons) {
+          setFilteredPokemons(
+            pokemons.filter((pokemon) =>
+              pokemon.name
+                .toLowerCase()
+                .includes(selectedPokemon.toLowerCase()),
+            ),
+          )
+        } else if (selectedAbility.length && abilities) {
+          if (
+            abilities.find(
+              (ability) =>
+                ability.name.toLowerCase() === selectedAbility.toLowerCase(),
+            )
+          ) {
+            fetchPokemonsWithAbility()
+          }
+        } else {
+          setFilteredPokemons([])
+        }
+      }, 200),
+      [selectedPokemon, selectedAbility, pokemons, abilities],
+    )
+
+    const resetFilter = () => {
+      setSelectedPokemon('')
+      setSelectedAbility('')
+    }
+
+    useEffect(() => {
+      const lsFilterByName = localStorage?.getItem(
+        LOCAL_STORAGE_KYES.gallery.filter.byName,
+      )
+      const lsFilterByAbility = localStorage?.getItem(
+        LOCAL_STORAGE_KYES.gallery.filter.byAbility,
+      )
+      if (lsFilterByName) setSelectedPokemon(lsFilterByName)
+      if (lsFilterByAbility) setSelectedAbility(lsFilterByAbility)
+    }, [])
+
+    useEffect(() => {
+      localStorage?.setItem(
+        LOCAL_STORAGE_KYES.gallery.filter.byName,
+        selectedPokemon,
+      )
+    }, [selectedPokemon])
+
+    useEffect(() => {
+      localStorage?.setItem(
+        LOCAL_STORAGE_KYES.gallery.filter.byAbility,
+        selectedAbility,
+      )
+    }, [selectedAbility])
+
+    useEffect(() => {
+      applyFilter()
+    }, [selectedPokemon, selectedAbility, pokemons, abilities, applyFilter])
+
+    useEffect(() => {
+      const data: NamedAPIResourceList = dataPokemons
+      if (data?.results) setPokemons(data.results)
+    }, [dataPokemons])
+
+    useEffect(() => {
+      const data: NamedAPIResourceList = dataAbilities
+      if (data?.results) setAbilities(data.results)
+    }, [dataAbilities])
+
+    useEffect(() => {
+      const data: Ability = dataPokemonsOfAbility
+
+      if (data?.pokemon) {
+        setFilteredPokemons(data.pokemon.map((item) => item.pokemon))
       }
-    } else {
-      setFilteredPokemons([])
-    }
-  }, 200), [selectedPokemon, selectedAbility, pokemons, abilities])
+    }, [dataPokemonsOfAbility, setFilteredPokemons])
 
-  const resetFilter = () => {
-    setSelectedPokemon('')
-    setSelectedAbility('')
-  }
+    useEffect(() => {
+      onFilter(filteredPokemons.length ? filteredPokemons : undefined)
+    }, [filteredPokemons, onFilter])
 
-  useEffect(() => {
-    const lsFilterByName = localStorage?.getItem(LOCAL_STORAGE_KYES.gallery.filter.byName)
-    const lsFilterByAbility = localStorage?.getItem(LOCAL_STORAGE_KYES.gallery.filter.byAbility)
-    if (lsFilterByName) setSelectedPokemon(lsFilterByName)
-    if (lsFilterByAbility) setSelectedAbility(lsFilterByAbility)
-  }, [])
-
-  useEffect(() => {
-    localStorage?.setItem(LOCAL_STORAGE_KYES.gallery.filter.byName, selectedPokemon)
-  }, [selectedPokemon])
-
-  useEffect(() => {
-    localStorage?.setItem(LOCAL_STORAGE_KYES.gallery.filter.byAbility, selectedAbility)
-  }, [selectedAbility])
-
-  useEffect(() => {
-    applyFilter()
-  }, [selectedPokemon, selectedAbility, pokemons, abilities, applyFilter])
-
-  useEffect(() => {
-    const data: NamedAPIResourceList = dataPokemons
-    if (data?.results) setPokemons(data.results)
-  }, [dataPokemons])
-
-  useEffect(() => {
-    const data: NamedAPIResourceList = dataAbilities
-    if (data?.results) setAbilities(data.results)
-  }, [dataAbilities])
-
-  useEffect(() => {
-    const data: Ability = dataPokemonsOfAbility
-
-    if (data?.pokemon) {
-      setFilteredPokemons(data.pokemon.map(item => item.pokemon))
-    }
-  }, [dataPokemonsOfAbility, setFilteredPokemons])
-
-  useEffect(() => {
-    onFilter(filteredPokemons.length ? filteredPokemons : undefined)
-  }, [filteredPokemons, onFilter])
-
-  return (
-    <div data-name='PokemonFilter' className='flex flex-wrap justify-between px-2 my-2 bg-gray-100 rounded'>
-      <div className='flex items-center -mx-2'>
-        {(pokemons || abilities) && <FilterIcon className='inline-block m-2 w-5 h-5 flex-grow sm:flex-grow-0' />}
-        {pokemons && (
-          <label className='inline-block m-2 w-1/2 sm:w-auto'>
-            <span className='sr-only'>Filter by name <span className='text-sm italic text-gray-400'>({pokemons.length})</span></span>
-            <Autocomplete
-              items={pokemons}
-              getItemValue={(item: NamedAPIResource) => item.name}
-              inputProps={{
-                className: 'input w-full',
-                placeholder: 'Filter by name'
-              }}
-              renderItem={(item: NamedAPIResource, isHighlighted) => (
-                <div
-                  key={item.url}
-                  className={classnames('dropdown-list-item', isHighlighted ? 'dropdown-list-item-selected' : undefined)}
-                >
-                  <span className='capitalize'>{item.name}</span>
-                </div>
-              )}
-              shouldItemRender={(item: NamedAPIResource, value: string) => item.name.toLowerCase().includes(value.toLowerCase())}
-              value={selectedPokemon}
-              onChange={(e) => {
-                setSelectedPokemon(e.target.value)
-                setSelectedAbility('')
-              }}
-              onSelect={val => {
-                setSelectedPokemon(val)
-                setSelectedAbility('')
-              }}
-              wrapperProps={{
-                className: 'w-full'
-              }}
-            />
-          </label>
-        )}
-        {abilities && (
-          <label className='inline-block m-2 w-1/2 sm:w-auto'>
-            <span className='sr-only'>Filter by ability <span className='text-sm italic text-gray-400'>({abilities.length})</span></span>
-            <Autocomplete
-              items={abilities}
-              getItemValue={(item: NamedAPIResource) => item.name}
-              inputProps={{
-                className: 'input w-full',
-                placeholder: 'Filter by ability'
-              }}
-              renderItem={(item: NamedAPIResource, isHighlighted) => (
-                <div
-                  key={item.url}
-                  className={classnames('dropdown-list-item', isHighlighted ? 'dropdown-list-item-selected' : undefined)}
-                >
-                  <span className='capitalize'>{item.name}</span>
-                </div>
-              )}
-              shouldItemRender={(item: NamedAPIResource, value: string) => item.name.toLowerCase().includes(value.toLowerCase())}
-              value={selectedAbility}
-              onChange={(e) => {
-                setSelectedPokemon('')
-                setSelectedAbility(e.target.value)
-              }}
-              onSelect={val => {
-                setSelectedPokemon('')
-                setSelectedAbility(val)
-              }}
-              wrapperProps={{
-                className: 'w-full'
-              }}
-            />
-          </label>
+    return (
+      <div
+        data-name="PokemonFilter"
+        className="flex flex-wrap justify-between px-2 my-2 bg-gray-100 rounded"
+      >
+        <div className="flex items-center -mx-2">
+          {(pokemons || abilities) && (
+            <FilterIcon className="inline-block m-2 w-5 h-5 flex-grow sm:flex-grow-0" />
+          )}
+          {pokemons && (
+            <label className="inline-block m-2 w-1/2 sm:w-auto">
+              <span className="sr-only">
+                Filter by name{' '}
+                <span className="text-sm italic text-gray-400">
+                  ({pokemons.length})
+                </span>
+              </span>
+              <Autocomplete
+                items={pokemons}
+                getItemValue={(item: NamedAPIResource) => item.name}
+                inputProps={{
+                  className: 'input w-full',
+                  placeholder: 'Filter by name',
+                }}
+                renderItem={(item: NamedAPIResource, isHighlighted) => (
+                  <div
+                    key={item.url}
+                    className={classnames(
+                      'dropdown-list-item',
+                      isHighlighted ? 'dropdown-list-item-selected' : undefined,
+                    )}
+                  >
+                    <span className="capitalize">{item.name}</span>
+                  </div>
+                )}
+                shouldItemRender={(item: NamedAPIResource, value: string) =>
+                  item.name.toLowerCase().includes(value.toLowerCase())
+                }
+                value={selectedPokemon}
+                onChange={(e) => {
+                  setSelectedPokemon(e.target.value)
+                  setSelectedAbility('')
+                }}
+                onSelect={(val) => {
+                  setSelectedPokemon(val)
+                  setSelectedAbility('')
+                }}
+                wrapperProps={{
+                  className: 'relative w-full z-10',
+                }}
+              />
+            </label>
+          )}
+          {abilities && (
+            <label className="inline-block m-2 w-1/2 sm:w-auto">
+              <span className="sr-only">
+                Filter by ability{' '}
+                <span className="text-sm italic text-gray-400">
+                  ({abilities.length})
+                </span>
+              </span>
+              <Autocomplete
+                items={abilities}
+                getItemValue={(item: NamedAPIResource) => item.name}
+                inputProps={{
+                  className: 'input w-full',
+                  placeholder: 'Filter by ability',
+                }}
+                renderItem={(item: NamedAPIResource, isHighlighted) => (
+                  <div
+                    key={item.url}
+                    className={classnames(
+                      'dropdown-list-item',
+                      isHighlighted ? 'dropdown-list-item-selected' : undefined,
+                    )}
+                  >
+                    <span className="capitalize">{item.name}</span>
+                  </div>
+                )}
+                shouldItemRender={(item: NamedAPIResource, value: string) =>
+                  item.name.toLowerCase().includes(value.toLowerCase())
+                }
+                value={selectedAbility}
+                onChange={(e) => {
+                  setSelectedPokemon('')
+                  setSelectedAbility(e.target.value)
+                }}
+                onSelect={(val) => {
+                  setSelectedPokemon('')
+                  setSelectedAbility(val)
+                }}
+                wrapperProps={{
+                  className: 'w-full',
+                }}
+              />
+            </label>
+          )}
+        </div>
+        {filteredPokemons.length > 0 && (
+          <div className="flex items-center -mx-2">
+            <div className="p-2">
+              {filteredPokemons.length}{' '}
+              {filteredPokemons.length > 1 ? 'Pokemons' : 'Pokemon'} found
+            </div>
+            <button className="link p-2" onClick={resetFilter}>
+              Reset
+            </button>
+          </div>
         )}
       </div>
-      {filteredPokemons.length > 0 && (
-        <div className='flex items-center -mx-2'>
-          <div className='p-2'>{filteredPokemons.length} {filteredPokemons.length > 1 ? 'Pokemons' : 'Pokemon'} found</div>
-          <button className='link p-2' onClick={resetFilter}>Reset</button>
-        </div>
-      )}
-    </div>
-  )
-})
+    )
+  },
+)
